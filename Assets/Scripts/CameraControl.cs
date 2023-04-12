c0f98ec8affde0f5d5e9e6cc689ccbb50b85c2dc
Vasco Santos <vascosantos.dev@gmail.com>
2023-04-12 17:45:43 +0100

    camera damp

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    // ammount to offset the center of the camera from the player
+    [SerializeField]
+    private float _playerOffset = 2f;
+
+    // treshold for the max speed the camera can follow the player
+    [SerializeField]
+    private float _cameraMaxThresholdScale = 1f;
     private void LateUpdate()
     {
         Vector3 playerPos = PlayerController.Instance.transform.position;
-        // smooth
-        Vector3 pos = new(transform.position.x, playerPos.y, transform.position.z);
+        // TO-DO smooth
+        float vel = PlayerController.Instance.Body.velocity.y;
+
+        float posY = Mathf.SmoothDamp(transform.position.y, playerPos.y + _playerOffset, ref vel, 0.1f*_cameraMaxThresholdScale) ;
+
+        Vector3 pos = new(transform.position.x, posY, transform.position.z);
         transform.position = pos;
     }
 }
