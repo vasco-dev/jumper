7ce8268fadc3e27da1284e7644a98690552f499b
202100642 <goncalo.sequeira@bee-eng.pt>
2023-04-12 18:03:40 +0100

    Tutorial Animation

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float maxTime = 60;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
